73a96b46588b693de08989485035e2700ce261e1
Aman Singh Thakur <amansinghthakur252002@gmail.com>
2024-06-26 18:33:40 +0530

    changes

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import AboutUsHeader from '../Component/AboutuUs/AboutUsHeader';
-// import ServicesCards from '../Component/Accomadations/ServicesCards'
-// import ServicesHeader from '../Component/Accomadations/ServicesHeader'
 
 function AboutUs() {
   return (
